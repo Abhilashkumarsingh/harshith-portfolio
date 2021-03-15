4c41801e53eaa3d260e1b2541536465da3893fb3
Abhilashkumarsingh <43114987+Abhilashkumarsingh@users.noreply.github.com>
2021-03-15 16:29:17 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+  
+	<!--bootstrap3 cdn ends here-->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="portfolio.css" type="Text/css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>PORTFOLIO HARSHITH</title>
+  </head>
+  <body>
+	
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><span style="color:white;font-size:50px;font-family:cursive">H</span><span style="color:yellow;font-size:25px">G</span>
+  <span style="color:white;font-size:15px" >n</span></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">HOME <span class="sr-only">(current)</span></a>
+      </li>
+      
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          CONTACT ME
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Contact Number : 9110601220</a>
+          <a class="dropdown-item" href="#">Email : harshithgowdan7@gmail </a>
+          <a class="dropdown-item" href="https://www.linkedin.com/in/harshith-gowda-n">LinkedIn</a>
+        </div>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#content">SKILLS</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#edu">EDUCATION QUALIFICATION</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#act">ACTIVITIES</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="resume.pdf" download>DOWNLOAD RESUME</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+  
+  
+  
+  
+  
+	</div>
+	<div class="icon-bar">
+	<a href="https://m.facebook.com/harshithgowda.powerstar" class="facebook"><i class="fa fa-facebook"></i></a> 
+	<a href="mailto('harshithgowdan7@gmail')"  class="google"><i class="fa fa-google"></i></a> 
+	<a href="https://www.linkedin.com/in/harshith-gowda-n" class="linkedin"><i class="fa fa-linkedin"></i></a>
+	<a href="https://www.instagram.com/gowda.harshith.hg10/" class="instagram"><i class="fa fa-instagram"></i></a>
+	 
+	</div>
+    <div class="container">
+	<div class="row" >
+		<div class="col-lg-6 col-md-6 col-sm-12" style="margin-top:2%;">
+			<img src="harshith.jfif" width="95%" height="95%">
+		</div>
+		<div class="col-lg-6 col-sm-12 col-md-6" style="padding:5% 12%">
+		Hello everyone, I AM<br/>
+		<span style="font-size:50px;">HARSHITH GOWDA</span><br/>
+		<strong>STUDENT @BIT</strong><br/>
+		<br/>
+		<span style="font-size:20 px;margin-top:2%;text-align:center;color:gray" id="style1">
+		I am a passionate programmer, full stack web developer coder, where my thinking skills can be employed
+		A highly organized and hard-working individual looking for a responsible position to gain practical experience.
+		</span>
+		<ul style="list-style-type:none;margin:2% 10%;">
+		<li style="font-size:15px"><i class="fa fa-phone" aria-hidden="true" style="margin-right:2%;"></i>9110601220</li>
+		<li style="font-size:15px"><i class="fa fa-envelope" aria-hidden="true" style="margin-right:2%;"></i>harshithgowdan7@gmail.com</li>
+		</ul>
+		</div>
+	</div>
+	<!--div ends-->	
+	</div>
+	
+	<div class="row" id="content">
+		<div class="col-sm-12 col-lg-4 col-md-4" style="padding:7% 3%;border:4px solid black;border-radius:10%;background-color:white;">
+		<div style="transform:rotate(-90deg);position:absolute;top:50%;left:80%;"><h2><strong>SKILLS</strong></h2></div>
+			<div class="skill" style="transform:rotate(15deg);">C</div>
+			<div class="skill" style="transform:rotate(3deg);">C++</div>
+			<div class="skill">JAVA</div>
+			<div class="skill" style="transform:rotate(2deg);">PYTHON</div>
+			<div class="skill" style="transform:rotate(5deg);">HTML</div>
+			<div class="skill" style="transform:rotate(1.5deg);">CSS</div>
+			<div class="skill">PANDAS</div>
+			<div class="skill">MATPLOTLIB</div>
+			<div class="skill">MYSQL</div>
+		</div>
+		
+		
+		
+		<div class="col-sm-12 col-lg-8 col-md-8" style="padding-right:10%;" id="projects">
+		<div style="transform:rotate(-90deg);position:absolute;top:50%;left:85%;"><h2><strong>PROJECTS</strong></h2></div>
+			<p style="transform:rotate(-10deg);"><strong>PROJECT MANAGEMENT SYSTEM</strong>, A DBMS based system for maintaining set of projects.</p>
+			<p style="transform:rotate(-5deg);"><strong>IRIS SPECIES DETECTOR</strong>,A Machine learning based system for detecting the species of iris plant</p>
+			<p style="transform:rotate(0deg);"><strong>BIT PLACEMENT ANALYSIS</strong>, used to analysis the placements in BIT college and 
+			it is implemented using python.</p>
+			<div style="margin-left:45%;"><a href="https://github.com/imharshith"><i class="fa fa-github" aria-hidden="true"></i> GITHUB</a></div>
+		</div>
+	
+	</div>
+	<!-- end of content-->
+	<div class="education" id="edu">
+	<div class="row"><div class="col-sm-12 col-lg-12 col-md-12">
+	<p style="transform:rotate(-90deg);font-size:40px;position:absolute;top:40%;left:90%;"><strong>EDUCATION</strong></p>
+	<table>
+	<tr>
+	<td><strong>DEGREE</strong></td><td><strong>INSTITUTE</strong></td><td><strong>SCORE</strong></td>
+	</tr>
+	<tr>
+	<td>CLASS 10 (SSLC)</td><td>EVERGREEN HIGH SCHOOL<BR/><span style="color:purple">2014-15</span></td><TD>90.56%</TD>
+	</tr>
+	
+	<tr>
+	<td>PUC</td><td>SRI MARUTHI PRE UNIVERSITY COLLEGE<BR/><span style="color:purple">2015-17</span></td><TD>94.83%</TD>
+	</tr>
+	
+	<tr>
+	<td>UG IN B.E. (COMPUTER SCIENCE)</td><td>BANGALORE INSTITUTE OF TECHNOLOGY<BR/><span style="color:purple">2017-21</span></td><TD>7.82 CGPA</TD>
+	</tr>
+	</table>
+	</div>
+	</div>
+	</div>
+	<!--end of education-->
+	<div class="activities" id="act"><div class="row"><div class="col-lg-12 col-sm-12 col-md-12">
+	<p style="font-size:40px;text-align:center;border-bottom:4px solid red"><strong>ACTIVITIES</strong></p>
+
+	<ul>
+	<li>Completed <strong>Programming for Everybody(Getting started with python) by University of Michigan</strong> course on coursera.</li>
+	<li>Participated in <strong>Data science with Python and Machine learning,</strong> a Two-day workshop by KIGYAN school of Data Science Pvt. Ltd.</li>
+	<li>Participated in Two-days workshop on<strong> Modern web application development using MEAN stack </strong> by Ramaiah Institute of Technology.</li>
+	<li>Certificate of excellence for<strong> IDEATHON</strong> to showcase ideas and opinions on how to make the surrounding a safe and better place for living</li>
+	<li>Certificate of participation and volunteering in planting saplings around Bangalore Institute of Technology by<strong>ECO CLUB, BIT</strong> Bangalore.</li>
+	</ul>
+	</div></div></div>
+	
+	<div style="width:100%;background-color:black;color:gray;text-align:center">copyright &#169 Team12</div>
+	
+	
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
